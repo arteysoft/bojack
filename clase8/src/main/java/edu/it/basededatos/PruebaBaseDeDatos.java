0a076acab1c2ce880f6b0f17b074a1baef82dfe0
andy <arteysoft@gmail.com>
2022-06-11 17:52:19 -0300

    clase #8

--- clase8/src/main/java/edu/it/basededatos/PruebaBaseDeDatos.java
+++ clase8/src/main/java/edu/it/basededatos/PruebaBaseDeDatos.java
@@ -30,6 +30,40 @@ public class PruebaBaseDeDatos {
 		stm.execute();
 		stm.close();
 	}
+	private void deletePorApellido(Connection conn, String apellido) throws Exception {
+		logger.info("Voy a borrar los alumnos con apellido: " + apellido);
+		
+		var strSQL = new StringBuilder()
+				.append("DELETE FROM alumnos ")
+				.append("WHERE apellido = ?")
+				.toString();
+	
+		var stm = conn.prepareStatement(strSQL);
+		stm.setString(1, apellido);
+		stm.execute();
+		
+		var filasAfectadas = stm.getUpdateCount();
+		logger.info("Total de filas afectadas: " + filasAfectadas);
+		
+		stm.close();
+	}
+	private void deletePorApellidoPrefijado(Connection conn, String apellidoIni) throws SQLException {
+		logger.info("Voy a borrar los alumnos con prefijo apellido: " + apellidoIni);
+		
+		var strSQL = new StringBuilder()
+				.append("DELETE FROM alumnos ")
+				.append("WHERE apellido LIKE ?")
+				.toString();
+	
+		var stm = conn.prepareStatement(strSQL);
+		stm.setString(1, "%" + apellidoIni + "%");
+		stm.execute();
+		
+		var filasAfectadas = stm.getUpdateCount();
+		logger.info("Total de filas afectadas: " + filasAfectadas);
+		
+		stm.close();
+	}
 	public void run() {
 		try {
 			logger.debug("Intentando con la parte static del Driver");
@@ -38,10 +72,15 @@ public class PruebaBaseDeDatos {
 			var connStr = "jdbc:mariadb://localhost:3306/bojack";
 			var conn = java.sql.DriverManager.getConnection(connStr, "root", "");
 			
+			/*
 			for (Integer x = 1; x <= 2000; x++) {
 				var alumno = UtilesAlumno.generarAlumnoRandom();
 				insertar(conn, alumno);
 			}
+			*/
+			
+			// deletePorApellido(conn, "Glover");
+			deletePorApellidoPrefijado(conn, "u");
 			
 			conn.close();
 			logger.debug("Cerrando base de datos");
