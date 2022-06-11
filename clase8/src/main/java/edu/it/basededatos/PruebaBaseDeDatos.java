35771ee350ceff79cca2c9031814fb8e4e85af15
andy <arteysoft@gmail.com>
2022-06-11 17:19:58 -0300

    create y luego r

--- clase8/src/main/java/edu/it/basededatos/PruebaBaseDeDatos.java
+++ clase8/src/main/java/edu/it/basededatos/PruebaBaseDeDatos.java
@@ -1,12 +1,35 @@
 package edu.it.basededatos;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 
 public class PruebaBaseDeDatos {
 	Logger logger = Logger.getLogger(getClass());
-	
+	private void insertar(Connection conn, Alumno alumno) throws SQLException {
+		logger.info("Voy a insertar el alumno");
+		logger.info(alumno.toString());
+		
+		/* java.sql.Statement */
+		var strSQL = new StringBuilder()
+				.append("INSERT alumnos ")
+				.append("(id, nombre, apellido, calle, calleNumero, estado, pais) ")
+				.append("VALUES (?,?,?,?,?,?,?)")
+				.toString();
+		
+		var stm = conn.prepareStatement(strSQL);
+		stm.setString(1, alumno.id);
+		stm.setString(2, alumno.nombre);
+		stm.setString(3, alumno.apellido);
+		stm.setString(4, alumno.calle);
+		stm.setString(5, alumno.calleNumero);
+		stm.setString(6, alumno.estado);
+		stm.setString(7, alumno.pais);
+		
+		stm.execute();
+		stm.close();
+	}
 	public void run() {
 		try {
 			logger.debug("Intentando con la parte static del Driver");
@@ -14,6 +37,12 @@ public class PruebaBaseDeDatos {
 			logger.debug("Abriendo base de datos");
 			var connStr = "jdbc:mariadb://localhost:3306/bojack";
 			var conn = java.sql.DriverManager.getConnection(connStr, "root", "");
+			
+			for (Integer x = 1; x <= 2000; x++) {
+				var alumno = UtilesAlumno.generarAlumnoRandom();
+				insertar(conn, alumno);
+			}
+			
 			conn.close();
 			logger.debug("Cerrando base de datos");
 		}
---
+++ clase8/src/main/java/edu/it/basededatos/UtilesAlumno.java
@@ -0,0 +1,21 @@
+package edu.it.basededatos;
+
+import java.util.UUID;
+
+import com.github.javafaker.Faker;
+
+public class UtilesAlumno {
+	public static Alumno generarAlumnoRandom() {
+		Faker fkr = new Faker();
+		
+		return new Alumno(
+				UUID.randomUUID().toString(),
+	            fkr.address().firstName(),
+	            fkr.address().lastName(),
+	            fkr.address().streetName(),
+	            fkr.address().streetAddressNumber(),
+	            fkr.address().state(),
+	            fkr.address().country()
+	        );
+	}
+}
