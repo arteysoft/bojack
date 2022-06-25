cc222a7834e6e512c5dc57c57dd859c63b1cf187
andy <arteysoft@gmail.com>
2022-06-25 17:19:41 -0300

    ejemplo transacciones

--- clase9/src/main/java/edu/it/transac/EjercicioTransacciones.java
+++ clase9/src/main/java/edu/it/transac/EjercicioTransacciones.java
@@ -2,6 +2,7 @@ package edu.it.transac;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Random;
 
 import org.apache.log4j.Logger;
 
@@ -20,16 +21,31 @@ public class EjercicioTransacciones {
 		stm.setInt(1, importe.intValue());
 		stm.execute();
 	}
+	private void generarErrorDeManeraRandom() {
+		if (new Random().nextBoolean()) {
+			logger.info("Voy a forzar un error");
+			throw new RuntimeException("Error forzaddo");
+		}
+		logger.info("No se genero error alguno");
+	}
 	public void run(ConnectionCreator connectionCreator) {
+		java.sql.Connection conn = null;
+		
 		try {
-			var conn = connectionCreator.crearConexion();
-			var importe = 100;
+			conn = connectionCreator.crearConexion();
+			var importe = 200;
+			conn.setAutoCommit(false);
 			realizarMovimiento(conn, importe);
+			generarErrorDeManeraRandom();
 			actualizarSaldo(conn, importe);
-			conn.close();
+			conn.commit();
 		}
 		catch (Exception ex) {
 			logger.error(ex);
+			try { conn.rollback(); } catch (Exception ex2) {}
+		}
+		finally {
+			try { conn.close(); } catch (Exception ex) {}
 		}
 	}
 }
